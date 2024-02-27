324ae21a7c1ae1de277fd53a408cb1b9b230ce5d
mnoslier <matthieu.noslier@ensae.fr>
2024-02-27 17:52:03 +0000

    Before dinner

--- Oaxaca.R
+++ Oaxaca.R
@@ -1,5 +1,3 @@
-##### First analyse with Oaxaca -----
-
 # Source:
 # Hlavac, Marek (2022). oaxaca: Blinder-Oaxaca Decomposition in R. R package version 0.1.5. https://CRAN.R-project.org/package=oaxaca
 
@@ -7,6 +5,9 @@ install.packages("oaxaca")
 
 eulfs_small$hwactual <- na_if(eulfs_small$hwactual, 99)
 eulfs_small$hwwish <- na_if(eulfs_small$hwwish, 99)
+
+
+##### First analysis with Oaxaca -----
 
 # The explanatory variables are not necessarily sufficient here
 # This is only a first attempt to use the package oaxaca
@@ -37,3 +38,27 @@ results$threefold$overall
 plot(results, components = c("endowments","coefficients"))
 
 # We should now take time to select the most relevant variables before further interpretation
+
+
+##### Second analysis with Oaxaca -----
+
+eulfs_women <- filter(eulfs_small, eulfs_small$sex == 2, eulfs_small$marstat == 1 | eulfs_small$marstat == 2 )
+eulfs_women$log_mar <- as.logical(eulfs_women$marstat - 1)
+# Comparison between married and single women
+results <- eulfs_women %>% oaxaca::oaxaca(formula = hwactual ~ age + hatlev1d + as.factor(degurba) | log_mar)
+
+results$y
+# Single women work slightly more than married women
+# 30.72 VS 30.45
+
+results$threefold$overall
+# This suggests that, of the 0.270 hours difference, 
+# approximately -0.215 can be attributed to group differences in endowments (i.e., age, education, living in a city or not),
+# 1.425 to differences in coefficients,
+# and the remaining -0.940 is accounted for by the interaction of the two.
+
+# We can visualize it thanks to a bar plot:
+plot(results, components = c("endowments","coefficients"))
+# Education is clearly the most decisive factor
+
+
